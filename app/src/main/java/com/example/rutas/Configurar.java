a7a86498e9500974b05ea57c2b026b7f6e22568f
danielp98 <s16120305@alumnos.itsur.edu.mx>
2020-05-11 21:14:44 -0500

    Actualización de código _ marcar puntos y dibujar ruta

--- app/src/main/java/com/example/rutas/Configurar.java
+++ app/src/main/java/com/example/rutas/Configurar.java
@@ -18,6 +18,7 @@ import android.view.ContextThemeWrapper;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,8 +43,8 @@ import java.util.List;
 
 public class Configurar extends AppCompatActivity {
     MapsActivity obj = new MapsActivity();
-    TextView latitud = findViewById(R.id.txtLatitud);
-    TextView longitud = findViewById(R.id.txtLongitud);
+    EditText latitud;
+    EditText longitud;
     LocationManager locationManager;
     Location location;
 
@@ -55,21 +56,17 @@ public class Configurar extends AppCompatActivity {
         longitud = findViewById(R.id.txtLongitud);
 
         Button btnConfigurar = findViewById(R.id.btnConfigurar);
-        //longitud es x
+
 
     }
 
 
     public void onClick(View view) {
-        LatLng destino = new LatLng(Double.parseDouble(longitud.toString()), Double.parseDouble(latitud.toString())); //Definimos el destino con las cajas de texto
+        //LatLng destino = new LatLng(Double.parseDouble(longitud.getText().toString()), Double.parseDouble(latitud.getText().toString())); //Definimos el destino con las cajas de texto
 
-        MarkerOptions marcadorDestino = new MarkerOptions();
-        marcadorDestino.position(destino);
-        marcadorDestino.title("Este es tu destino");
-        //marcadorDestino.icon(BitmapDescriptorFactory.fromBitmap(resizeMapIcons("ic_marcador_destino",80,80)));
-        obj.mMap.addMarker(marcadorDestino);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        /*locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
@@ -79,228 +76,20 @@ public class Configurar extends AppCompatActivity {
         }
         location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        LatLng origen = new LatLng(location.getLongitude(), location.getLatitude());
+        LatLng origen = new LatLng(location.getLongitude(), location.getLatitude());*/
 
-        String url = obtenerDireccionesURL(origen, destino);
-        DownloadTask downloadTask = new DownloadTask();
-        downloadTask.execute(url);
+
 
         //mandar a la otra actividad
         Intent intent = new Intent(this, MapsActivity.class);
+
+        intent.putExtra("longitud_destino", Double.parseDouble(longitud.getText().toString()));
+        intent.putExtra("latitud_destino", Double.parseDouble(latitud.getText().toString()));
+
         startActivity(intent);
 
     }
 
-    private String obtenerDireccionesURL(LatLng origin,LatLng dest){
 
-        String str_origin = "origin="+origin.latitude+","+origin.longitude;
-
-        String str_dest = "destination="+dest.latitude+","+dest.longitude;
-
-        String sensor = "sensor=false";
-
-        String parameters = str_origin+"&"+str_dest+"&"+sensor;
-
-        String output = "json";
-
-        String url = "https://maps.googleapis.com/maps/api/directions/"+output+"?"+parameters;
-
-        return url;
-    }
-
-    private class DownloadTask extends AsyncTask<String, Void, String> {
-
-        @SuppressLint("LongLogTag")
-        @Override
-        protected String doInBackground(String... url) {
-
-            String data = "";
-
-            try{
-                data = downloadUrl(url[0]);
-            }catch(Exception e){
-                Log.d("ERROR AL OBTENER INFO DEL WS",e.toString());
-            }
-            return data;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            ParserTask parserTask = new ParserTask();
-
-            parserTask.execute(result);
-        }
-    }
-
-    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String,String>>> >{
-
-        @Override
-        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
-
-            JSONObject jObject;
-            List<List<HashMap<String, String>>> routes = null;
-
-            try{
-                jObject = new JSONObject(jsonData[0]);
-                DirectionsJSONParser parser = new DirectionsJSONParser();
-
-                routes = parser.parse(jObject);
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-            return routes;
-        }
-
-        @Override
-        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
-            ArrayList<LatLng> points = null;
-            PolylineOptions lineOptions = null;
-            MarkerOptions markerOptions = new MarkerOptions();
-
-            for(int i=0;i<result.size();i++){
-                points = new ArrayList<LatLng>();
-                lineOptions = new PolylineOptions();
-
-                List<HashMap<String, String>> path = result.get(i);
-
-                for(int j=0;j<path.size();j++){
-                    HashMap<String,String> point = path.get(j);
-
-                    double lat = Double.parseDouble(point.get("lat"));
-                    double lng = Double.parseDouble(point.get("lng"));
-                    LatLng position = new LatLng(lat, lng);
-
-                    points.add(position);
-                }
-
-                lineOptions.addAll(points);
-                lineOptions.width(4);
-                lineOptions.color(Color.rgb(0,0,255));
-            }
-            if(lineOptions!=null) {
-                //mGoogleMap.addPolyline(lineOptions);
-                obj.mMap.addPolyline(lineOptions);
-            }
-        }
-    }
-
-    public class DirectionsJSONParser {
-
-        public List<List<HashMap<String,String>>> parse(JSONObject jObject){
-
-            List<List<HashMap<String, String>>> routes = new ArrayList<List<HashMap<String,String>>>() ;
-            JSONArray jRoutes = null;
-            JSONArray jLegs = null;
-            JSONArray jSteps = null;
-
-            try {
-
-                jRoutes = jObject.getJSONArray("routes");
-
-                for(int i=0;i<jRoutes.length();i++){
-                    jLegs = ( (JSONObject)jRoutes.get(i)).getJSONArray("legs");
-                    List path = new ArrayList<HashMap<String, String>>();
-
-                    for(int j=0;j<jLegs.length();j++){
-                        jSteps = ( (JSONObject)jLegs.get(j)).getJSONArray("steps");
-
-                        for(int k=0;k<jSteps.length();k++){
-                            String polyline = "";
-                            polyline = (String)((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
-                            List<LatLng> list = decodePoly(polyline);
-
-                            for(int l=0;l<list.size();l++){
-                                HashMap<String, String> hm = new HashMap<String, String>();
-                                hm.put("lat", Double.toString(((LatLng)list.get(l)).latitude) );
-                                hm.put("lng", Double.toString(((LatLng)list.get(l)).longitude) );
-                                path.add(hm);
-                            }
-                        }
-                        routes.add(path);
-                    }
-                }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }catch (Exception e){
-            }
-
-            return routes;
-        }
-
-        private List<LatLng> decodePoly(String encoded) {
-
-            List<LatLng> poly = new ArrayList<LatLng>();
-            int index = 0, len = encoded.length();
-            int lat = 0, lng = 0;
-
-            while (index < len) {
-                int b, shift = 0, result = 0;
-                do {
-                    b = encoded.charAt(index++) - 63;
-                    result |= (b & 0x1f) << shift;
-                    shift += 5;
-                } while (b >= 0x20);
-                int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-                lat += dlat;
-
-                shift = 0;
-                result = 0;
-                do {
-                    b = encoded.charAt(index++) - 63;
-                    result |= (b & 0x1f) << shift;
-                    shift += 5;
-                } while (b >= 0x20);
-                int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-                lng += dlng;
-
-                LatLng p = new LatLng((((double) lat / 1E5)),
-                        (((double) lng / 1E5)));
-                poly.add(p);
-            }
-
-            return poly;
-        }
-    }
-
-    private String downloadUrl(String strUrl) throws IOException {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
-        try{
-            URL url = new URL(strUrl);
-
-            // Creamos una conexion http
-            urlConnection = (HttpURLConnection) url.openConnection();
-
-            // Conectamos
-            urlConnection.connect();
-
-            // Leemos desde URL
-            iStream = urlConnection.getInputStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-
-            StringBuffer sb = new StringBuffer();
-
-            String line = "";
-            while( ( line = br.readLine()) != null){
-                sb.append(line);
-            }
-
-            data = sb.toString();
-
-            br.close();
-
-        }catch(Exception e){
-            Log.d("Exception", e.toString());
-        }finally{
-            iStream.close();
-            urlConnection.disconnect();
-        }
-        return data;
-    }
 
 }
--- app/src/main/java/com/example/rutas/MapsActivity.java
+++ app/src/main/java/com/example/rutas/MapsActivity.java
@@ -1,9 +1,20 @@
 package com.example.rutas;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -12,10 +23,29 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     public GoogleMap mMap;
+    LocationManager locationManager;
+    Location location;
+    LatLng destino;
+    LatLng origen;
+    Double longitud_destino, latitud_destino;
+    MarkerOptions options = new MarkerOptions();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +55,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        longitud_destino = getIntent().getDoubleExtra("longitud_destino", 0.0);
+        latitud_destino = getIntent().getDoubleExtra("latitud_destino", 0.0);
+
+        destino = new LatLng(longitud_destino, latitud_destino);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            Toast.makeText(this, "Se requiere definir los permisos", Toast.LENGTH_SHORT).show();
+
+            return;
+        }
+
+        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        //origen = new LatLng(location.getLongitude(), location.getLatitude());
+
     }
 
 
@@ -50,5 +99,180 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         ///////////////
         //mMap.setMyLocationEnabled(true);
 
+        final Context context = this;
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                    mMap.clear();
+                    Intent intentConfigurar = new Intent(context, Configurar.class);
+                    startActivity(intentConfigurar);
+            }
+        });
+
+        //origen = new LatLng(20.168975, -100.162587);
+        origen = new LatLng(location.getLatitude(), location.getLongitude());
+
+
+        Toast.makeText(this, location.getLongitude() + ", " + location.getLatitude() , Toast.LENGTH_SHORT).show();
+
+        // Creating MarkerOptions
+
+
+
+        // Setting the position of the marker
+        options.position(destino);
+        mMap.addMarker(options);
+
+        options.position(origen);
+        mMap.addMarker(options);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(origen, 16));
+
+        // Getting URL to the Google Directions API
+        String url = obtenerDireccionesURL(origen, destino);
+
+        DownloadTask downloadTask = new DownloadTask();
+
+        // Start downloading json data from Google Directions API
+        downloadTask.execute(url);
+
     }
+
+    private String obtenerDireccionesURL(LatLng origin,LatLng dest){
+
+        String str_origin = "origin="+origin.latitude+","+origin.longitude;
+
+        String str_dest = "destination="+dest.latitude+","+dest.longitude;
+
+        //String sensor = "sensor=false";
+
+        String key = "key=AIzaSyDZx8ah9aVIZ57h7k1-V2DnivCSKNkLtXA";
+
+        String parameters = str_origin+"&"+str_dest+"&"+key;
+
+        String output = "json";
+
+        String url = "https://maps.googleapis.com/maps/api/directions/"+output+"?"+parameters;
+
+        //"http://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "?key= "+"ADD YOUR KEY HERE" + "&sensor=false";
+
+        return url;
+    }
+
+    private class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @SuppressLint("LongLogTag")
+        @Override
+        protected String doInBackground(String... url) {
+
+            String data = "";
+
+            try{
+                data = downloadUrl(url[0]);
+            }catch(Exception e){
+                Log.d("ERROR AL OBTENER INFO DEL WS",e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+            parserTask.execute(result);
+        }
+    }
+
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String,String>>> >{
+
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try{
+                jObject = new JSONObject(jsonData[0]);
+                DirectionsJSONParser parser = new DirectionsJSONParser();
+
+                routes = parser.parse(jObject);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points = null;
+            PolylineOptions lineOptions = null;
+            MarkerOptions markerOptions = new MarkerOptions();
+
+            for(int i=0;i<result.size();i++){
+                points = new ArrayList<LatLng>();
+                lineOptions = new PolylineOptions();
+
+                List<HashMap<String, String>> path = result.get(i);
+
+                for(int j=2;j<path.size();j++){
+                    HashMap<String,String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                lineOptions.addAll(points);
+                lineOptions.width(4);
+                lineOptions.color(Color.rgb(0,0,255));
+            }
+            if(lineOptions!=null) {
+                mMap.addPolyline(lineOptions);
+            }
+        }
+    }
+
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try{
+            URL url = new URL(strUrl);
+
+            // Creamos una conexion http
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Conectamos
+            urlConnection.connect();
+
+            // Leemos desde URL
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while( ( line = br.readLine()) != null){
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        }catch(Exception e){
+            Log.d("Exception", e.toString());
+        }finally{
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
 }
